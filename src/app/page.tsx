63fd592c66025e20fbf938d4e32fa6f4c8f495b6
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2025-09-01 14:40:24 -0300

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       </div>
       <div id="projetos-area">
         <div id="projetos">
-          <a className="projeto" id="numero1" href={"/Projeto1"}>Projeto1</a>
-          <a className="projeto" id="numero2" href={"/Projeto2"}>Projeto2</a>
+          <a className="projeto" id="numero1" href={"/projeto1"}>Projeto1</a>
+          <a className="projeto" id="numero2" href={"/projeto2"}>Projeto2</a>
           <a className="projeto" id="">...</a>
           <a className="projeto" id="">...</a>
           <a className="projeto" id="">...</a>
